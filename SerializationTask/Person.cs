eb66f0fb58ddea3a06af4a26477256df2f0dd44a
zavox <andras.kesik@gmail.com>
2016-05-02 11:11:05 +0200

    serialization v1

---
+++ SerializationTask/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+
+namespace SerializationTask
+{
+    [Serializable]
+    class Person
+    {
+        string name;
+        int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}\tAge: {age}";
+        }
+    }
+}
---
+++ SerializationTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace SerializationTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("András", 25);
+            Console.WriteLine(p1);
+            Serialize(p1);
+            Person p2 = Deserialize();
+            Console.WriteLine(p2);
+        }
+
+        private static void Serialize(Person sp)
+        {
+            FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, sp);
+            fs.Close();
+        }
+
+        private static Person Deserialize()
+        {
+            Person dsp = new Person();
+            FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            dsp = (Person)bf.Deserialize(fs);
+            fs.Close();
+            return dsp;
+        }
+    }
+}
